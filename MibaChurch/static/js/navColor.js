006cc12d2a7b753eef4ac7ee7956d8ddc32d0284
jobin joseph <jobinjoseph1018@gmail.com>
2020-07-02 23:15:12 +0530

    fixed navbar issue by adding toggle height header

--- MibaChurch/static/js/navColor.js
+++ MibaChurch/static/js/navColor.js
@@ -1,10 +1,14 @@
 const navBar = document.querySelector('.navbar'),
-            navBarCollapse = document.querySelector('.navbar-collapse'),
-            navTogglerBtn = document.querySelector('.navbar-toggler');
+    navBarCollapse = document.querySelector('.navbar-collapse'),
+    navTogglerBtn = document.querySelector('.navbar-toggler'),
+    nonHomeHeader = document.querySelector('.non-home-header'),
+    galleryDetailHeader = document.querySelector('.gallery-detail-header');
 
-        navTogglerBtn.addEventListener('click', () => {
-            navBar.classList.toggle('navbar-collapse-bg')
-        });
-        navTogglerBtn.addEventListener('click', () => {
-            navBarCollapse.classList.toggle('navbar-collapse-bg')
-        });
+navTogglerBtn.addEventListener('click', () => {
+    navBar.classList.toggle('navbar-collapse-bg');
+    navBarCollapse.classList.toggle('navbar-collapse-bg');
+    nonHomeHeader.classList.toggle('active-header');
+    hubDetailHeader.classList.toggle('active-header');
+    galleryDetailHeader.classList.toggle('active-header');
+
+});
