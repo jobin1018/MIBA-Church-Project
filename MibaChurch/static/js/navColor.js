d355e6dd8189057dc59de612b87149961faa8683
jobin joseph <jobinjoseph1018@gmail.com>
2020-07-03 11:13:17 +0530

    added my custom navbar

--- MibaChurch/static/js/navColor.js
+++ MibaChurch/static/js/navColor.js
@@ -1,14 +1,15 @@
-const navBar = document.querySelector('.navbar'),
-    navBarCollapse = document.querySelector('.navbar-collapse'),
-    navTogglerBtn = document.querySelector('.navbar-toggler'),
-    nonHomeHeader = document.querySelector('.non-home-header'),
-    galleryDetailHeader = document.querySelector('.gallery-detail-header');
 
-navTogglerBtn.addEventListener('click', () => {
-    navBar.classList.toggle('navbar-collapse-bg');
-    navBarCollapse.classList.toggle('navbar-collapse-bg');
+// MenuToggle script 
+const mainNav = document.querySelector('.nav-container'),
+    burgerMenu = document.querySelector('.burger-menu'),
+    nonHomeHeader = document.querySelector('.non-home-header'),
+    galleryDetailHeader = document.querySelector('.gallery-detail-header'),
+    hubDetailHeader = document.querySelector('.gallery-detail-header');
+
+
+burgerMenu.addEventListener('click', function () {
+    mainNav.classList.toggle('open');
     nonHomeHeader.classList.toggle('active-header');
     hubDetailHeader.classList.toggle('active-header');
     galleryDetailHeader.classList.toggle('active-header');
-
 });
